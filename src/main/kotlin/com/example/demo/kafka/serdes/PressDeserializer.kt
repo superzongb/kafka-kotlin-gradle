50761f6b0046ef6c0286ca3bd76132676473cd3a
bin.zong <superzongb@163.com>
2020-03-04 11:04:27 +0800

    add some new process, just send piano notes.

---
+++ src/main/kotlin/com/example/demo/kafka/serdes/PressDeserializer.kt
@@ -0,0 +1,17 @@
+package com.example.demo.kafka.serdes
+
+import com.example.demo.pojo.Press
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.kafka.common.serialization.Deserializer
+
+class PressDeserializer : Deserializer<Press> {
+    var kafkaAvroDeserializer: KafkaAvroDeserializer = KafkaAvroDeserializer()
+
+    override fun deserialize(topic: String?, data: ByteArray?): Press {
+        return kafkaAvroDeserializer.deserialize(topic, data) as Press
+    }
+
+    override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {
+        kafkaAvroDeserializer.configure(configs, isKey)
+    }
+}
