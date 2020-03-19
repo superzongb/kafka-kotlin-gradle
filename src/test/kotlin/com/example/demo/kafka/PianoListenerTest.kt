98bd559878a1935d74a335f51b9159fc97711400
bin.zong <superzongb@163.com>
2020-03-19 10:02:09 +0800

    add some new process, just send piano notes.

--- src/test/kotlin/com/example/demo/kafka/PianoListenerTest.kt
+++ src/test/kotlin/com/example/demo/kafka/PianoListenerTest.kt
@@ -3,57 +3,36 @@ package com.example.demo.kafka
 
 import com.example.demo.DemoApplication
 import com.example.demo.pojo.Press
-import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient
-import io.confluent.kafka.serializers.*
 import org.apache.kafka.clients.consumer.Consumer
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecords
-import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
-import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.kafka.test.utils.KafkaTestUtils
-import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [DemoApplication::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext
+@Import(TestEmbeddedKafkaConfiguration::class)
 @EmbeddedKafka(topics = ["Piano"], bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 class PianoListenerTest {
 
     @Autowired
-    lateinit var embeddedKafka: EmbeddedKafkaBroker
+    lateinit var kafkaTemplate: KafkaTemplate<String, Any>
 
-
-
-    val registryClient: MockSchemaRegistryClient = MockSchemaRegistryClient()
+    @Autowired
+    lateinit var consumerFactory: DefaultKafkaConsumerFactory<String, Any>
 
     @BeforeEach
     internal fun setUp() {
-        var producerProps =
-            KafkaTestUtils.producerProps(embeddedKafka)
-        producerProps[AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS] = "true"
-        producerProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://127.0.0.1:8081"
-        var keyDeserializer = StringSerializer()
-        var valueDeserializer = KafkaAvroSerializer(registryClient)
-        valueDeserializer.configure(producerProps, false)
-        val pf: DefaultKafkaProducerFactory<String, Any> =
-            DefaultKafkaProducerFactory(producerProps, keyDeserializer, valueDeserializer)
-        var template = KafkaTemplate(pf)
-
-
-        template.send(
+        kafkaTemplate.send(
             "Piano", Press(
                 Press.PIANO_NOTES[0],
                 System.currentTimeMillis(),
@@ -64,28 +43,9 @@ class PianoListenerTest {
 
     @Test
     fun test_listener() {
-        var consumerProps = KafkaTestUtils.consumerProps(
-            "test",
-            "true",
-            this.embeddedKafka
-        )
-        consumerProps[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-        consumerProps[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = "true"
-        consumerProps[AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS] = "true"
-        consumerProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://127.0.0.1:8081"
-        var keyDeserializer = StringDeserializer()
-        var valueDeserializer = KafkaAvroDeserializer(registryClient)
-        valueDeserializer.configure(consumerProps, false)
-
-        var consumer: Consumer<String, Any> = DefaultKafkaConsumerFactory(
-            consumerProps,
-            keyDeserializer,
-            valueDeserializer
-        ).createConsumer()
-
-        this.embeddedKafka.consumeFromEmbeddedTopics(consumer, "Piano")
-        val replies: ConsumerRecords<String, Any>? =
-            KafkaTestUtils.getRecords(consumer)
+        var consumer: Consumer<String, Any> = consumerFactory.createConsumer()
+        consumer.subscribe(listOf("Piano"))
+        val replies: ConsumerRecords<String, Any>? = consumer.poll(10000)
         assertTrue(replies!!.count() >= 1)
     }
 }
---
+++ src/test/kotlin/com/example/demo/kafka/TestEmbeddedKafkaConfiguration.kt
@@ -0,0 +1,71 @@
+package com.example.demo.kafka
+
+import io.confluent.kafka.schemaregistry.client.MockSchemaRegistryClient
+import io.confluent.kafka.serializers.*
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.kafka.core.*
+import org.springframework.kafka.test.EmbeddedKafkaBroker
+import org.springframework.kafka.test.utils.KafkaTestUtils
+
+@Configuration
+open class TestEmbeddedKafkaConfiguration {
+    @Autowired
+    lateinit var embeddedKafka: EmbeddedKafkaBroker
+
+    private val registryClient: MockSchemaRegistryClient = MockSchemaRegistryClient()
+
+    @Bean("testConsumerFactory")
+    @Primary
+    open fun consumerFactory(): ConsumerFactory<String, Any> {
+        val valueSerializer = KafkaAvroDeserializer(registryClient)
+        valueSerializer.configure(consumerProps(), false)
+        return DefaultKafkaConsumerFactory(
+            consumerProps(),
+            StringDeserializer(),
+            valueSerializer
+        )
+    }
+
+    private fun consumerProps(): Map<String, Any> {
+        val consumerProps = KafkaTestUtils.consumerProps(
+            "test",
+            "true",
+            this.embeddedKafka
+        )
+        consumerProps[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
+        consumerProps[KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG] = "true"
+        consumerProps[AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS] = "true"
+        consumerProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://127.0.0.1:8081"
+        return consumerProps
+    }
+
+    //创建生产者工厂
+    @Bean("testProducerFactory")
+    @Primary
+    open fun producerFactory(): ProducerFactory<String, Any> {
+        val valueDeserializer = KafkaAvroSerializer(registryClient)
+        valueDeserializer.configure(producerProps(), false)
+        return DefaultKafkaProducerFactory(producerProps(), StringSerializer(), valueDeserializer)
+    }
+
+    private fun producerProps(): Map<String, Any> {
+        val producerProps =
+            KafkaTestUtils.producerProps(embeddedKafka)
+        producerProps[AbstractKafkaAvroSerDeConfig.AUTO_REGISTER_SCHEMAS] = "true"
+        producerProps[KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG] = "http://127.0.0.1:8081"
+        return producerProps
+    }
+
+    //kafkaTemplate实现了Kafka发送接收等功能
+    @Bean("testKafkaTemplate")
+    @Primary
+    open fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+}
